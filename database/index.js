167709e2cde69294c8761364000fb34a6a432c16
Melanie DeWitt <melaniebd1986@gmail.com>
2021-03-03 21:37:04 -0700

    added mongodb schema and basic functions

--- database/index.js
+++ database/index.js
@@ -3,8 +3,55 @@ mongoose.connect('mongodb://localhost/spacebnb');
 
 
 let reviewsSchema = mongoose.Schema({
-  Reviews schema {
     property: Number,
     reviews: []
   }
 })
+
+let Reviews = mongoose.model('Reviews', reviewsSchema);
+
+let saveProperty = (propertyObj) => {
+  let property = new Reviews({
+    property = propertyObj.property,
+    reviews = []
+  })
+  property.save()
+  .then(result => {
+    mongoose.connection.close();
+  })
+}
+
+let saveReview = (reviewObj) => {
+  let review = {
+    date: {
+      month: reviewObj.date.month,
+      year: reviewObj.date.year
+    },
+    name: reviewObj.name,
+    image: reviewObj.image,
+    lastminute: reviewObj.lastminute,
+    text: reviewObj.text,
+    stars: {
+      cleanliness: reviewObj.stars.cleanliness,
+      communication: reviewObj.stars.communication,
+      checkin: reviewObj.stars.checkin,
+      accuracy: reviewObj.stars.accuracy,
+      location: reviewObj.stars.location,
+      value: reviewObj.stars.value
+    }
+  };
+  let db = mongoose.connection;
+  db.Reviews.update(
+    {property: reviewObj.property},
+    {$push: {reviews: review}}
+  )
+  .then(result => {
+    mongoose.connection.close();
+  })
+}
+
+let getReviews = (propertyID) => {
+  mongoose.connect('mongodb://localhost/spacebnb');
+  let db = mongoose.connection;
+  return Reviews.findOne({property: propertyID})
+}
